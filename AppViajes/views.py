cbe728ada25abc71d023de83d27a5f3ddc363875
Lucrecia Vettori <lucrecia.vettori.riga@gmail.com>
2022-03-03 19:43:49 -0300

    Agregando boton de busqueda

--- AppViajes/views.py
+++ AppViajes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,7 +8,14 @@ def inicio(request):
     return render (request, 'AppViajes/inicio.html')
 
 def pages(request):
+    busqueda=request.GET.get("buscar")
     posts =Post.objects.filter()
+    
+    if busqueda:
+        posts = Post.objects.filter(
+            Q(titulo__icontains = busqueda))
+
+        
     return render(request, 'AppViajes/index.html', {'posts':posts})
 
 def detallepost(request, slug):
