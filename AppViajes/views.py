730fe90252a6991487b14e649f29cf03db8a4b09
Lucrecia Vettori <lucrecia.vettori.riga@gmail.com>
2022-03-03 13:23:57 -0300

    Creando templates

--- AppViajes/views.py
+++ AppViajes/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
 def inicio(request):
-    return render(request, 'AppViajes/index.html')
+    return render (request, 'AppViajes/inicio.html')
+
+def pages(request):
+    posts =Post.objects.filter()
+    return render(request, 'AppViajes/index.html', {'posts':posts})
+
+def detallepost(request, slug):
+    post =Post.objects.get(
+        slug = slug
+    )
+    return render(request, 'AppViajes/post.html', {'Detalle_post':post})
