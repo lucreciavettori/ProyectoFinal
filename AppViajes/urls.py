0657016ce86d4b54e8d177e0dffbd159945ae131
Emmanuel Mira <emmanuelmira@gmail.com>
2022-03-05 12:58:40 -0300

    se crea la app para el manejo de cuentas

--- AppViajes/urls.py
+++ AppViajes/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from AppViajes.views import *
+from AppAccounts.views import login_request, register
+
 app_name = 'AppViajes' 
 
 #AppViajes='accounts'
@@ -8,5 +12,8 @@ urlpatterns = [
     path('inicio/',inicio ,name="Inicio"),
     path('pages/',pages ,name="Pages"),
     path('<str:slug>/', detallepost,name="Detalle_post"),
+    path('account/login/', login_request,name="Login"),
+    path('account/logout/', LogoutView.as_view(template_name='AppAccounts/logout.html'),name="Logout"),
+    path('account/register/', register,name="Register"),
 
 ]
