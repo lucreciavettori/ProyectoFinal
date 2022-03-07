2ca2dc463bfb39a81bb332b1517e9554f30f1c8c
Emmanuel Mira <emmanuelmira@gmail.com>
2022-03-07 08:37:44 -0300

    vincular botón log in

--- ProyectoBlog/urls.py
+++ ProyectoBlog/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppViajes/', include('AppViajes.urls')),
-    
+        
 ]
 
 if settings.DEBUG:
