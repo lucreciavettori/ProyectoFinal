0841226c068a4b8750704417be38e130fec3cfcb
Lucrecia Vettori <lucrecia.vettori.riga@gmail.com>
2022-03-13 13:31:26 -0300

    mejorando vista pages

--- AppViajes/urls.py
+++ AppViajes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from AppViajes.views import *
 from AppAccounts.views import login_request, register
+
    
 
 app_name = 'AppViajes' 
@@ -17,6 +18,7 @@ urlpatterns = [
     path('account/login/', login_request,name="Login"),
     path('account/logout/', LogoutView.as_view(template_name='AppAccounts/logout.html'),name="Logout"),
     path('account/register/', register,name="Register"),
+
    
     
 ]
